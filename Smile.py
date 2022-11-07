c5402ed6b461afa1c01a94fe88ca40120ef5e4a7
Romariok <ronya-ko@yandex.ru>
2022-11-07 18:55:08 +0300

    added choice of smile

--- Smile.py
+++ Smile.py
@@ -8,15 +8,26 @@ def readfile(s):
     tmp.close()
     return tmp1
 
-smile = r'=-\|'
+print("Введите тип взаимодействия с программой\n1 - Встроенные тесты\n2 - Саммостоятельное введений смайлика и текста")
+k = int(input())
+if k == 1:
+    smile = r'=-|'
 
-t1 = re.findall(smile, readfile("Smile_tests/Smile_test1.txt"))
-t2 = re.findall(smile, readfile("Smile_tests/Smile_test2.txt"))
-t3 = re.findall(smile, readfile("Smile_tests/Smile_test3.txt"))
-t4 = re.findall(smile, readfile("Smile_tests/Smile_test4.txt"))
-t5 = re.findall(smile, readfile("Smile_tests/Smile_test5.txt"))
-print("Количество вхождений смайлика в тексте 1 = ", len(t1))
-print("Количество вхождений смайлика в тексте 2 = ", len(t2))
-print("Количество вхождений смайлика в тексте 3 = ", len(t3))
-print("Количество вхождений смайлика в тексте 4 = ", len(t4))
-print("Количество вхождений смайлика в тексте 5 = ", len(t5))
+    t1 = re.findall(smile, readfile("Smile_tests/Smile_test1.txt"))
+    t2 = re.findall(smile, readfile("Smile_tests/Smile_test2.txt"))
+    t3 = re.findall(smile, readfile("Smile_tests/Smile_test3.txt"))
+    t4 = re.findall(smile, readfile("Smile_tests/Smile_test4.txt"))
+    t5 = re.findall(smile, readfile("Smile_tests/Smile_test5.txt"))
+    print("Количество вхождений смайлика в тексте 1 = ", len(t1))
+    print("Количество вхождений смайлика в тексте 2 = ", len(t2))
+    print("Количество вхождений смайлика в тексте 3 = ", len(t3))
+    print("Количество вхождений смайлика в тексте 4 = ", len(t4))
+    print("Количество вхождений смайлика в тексте 5 = ", len(t5))
+
+else:
+    print("Введите ваш смайлик")
+    smile = r''.join(str(input()))
+    print("Введите путь вашего теста в формате .txt")
+    path = str(input())
+    t1 = re.findall(smile, readfile("Smile_tests/Smile_test1.txt"))
+    print("Количество вхождений смайлика в тексте = ", len(t1))
